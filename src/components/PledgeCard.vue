bca3c2223a9a3d8ea4b0030405803ad20f3c62e1
githukelvin <kelvingithu019@gmail.com>
2023-07-14 19:28:15 +0300

    todays last commit

--- src/components/PledgeCard.vue
+++ src/components/PledgeCard.vue
@@ -5,32 +5,162 @@
 <template>
  <div class="top">
     <div class="up com">
-        <div class="radio">
-
-
-          </div>
+        <div class="radio"></div>
           <div class="info">
-          <h1></h1>
-          <p></p> 
+            <div class="pledge">
+              <div class="more">
+                <h2> Pledge with no reward</h2>
+              </div>
+            </div>
+            <p>  Choose to support us without a reward if you simply believe in our project. As a backer, 
+    you will be signed up to receive product updates via email.</p> 
           </div>
     </div>
-    <div class="low com">
-      <input type="text" name="text" id="text" placeholder="Enter your pledge">
-      <div class="controls">
-          <div class="amount">
-            <p>$</p>
-            <h2>25</h2>
-          </div>
-          <button class="" id="con">Continue</button>
-      </div>
-    </div>
+ 
 
 
   </div>
+<div class="list">
+     <div class="top">
+        <div class="up com">
+            <div class="radio"></div>
+              <div class="info">
+                <div class="pledge">
+                  <div class="more">
+                    <h2></h2>
+                    <p></p>
+                  </div>
+                  <h1>
+                    <span></span>
+                  </h1>
+                </div>
+                <p></p> 
+              </div>
+        </div>
+        <div class="low com">
+          <input type="text" name="text" id="text" placeholder="Enter your pledge">
+          <div class="controls">
+              <div class="amount">
+                <p>$</p>
+                <h2>25</h2>
+              </div>
+              <button class="" id="con">Continue</button>
+          </div>
+      
+      </div>
+
+
+      </div>
+       <div class="top">
+        <div class="up com">
+            <div class="radio"></div>
+              <div class="info">
+                <div class="pledge">
+                  <div class="more">
+                    <h2>Bamboo Stand</h2>
+                    <p>Pledge $25 or more</p>
+                  </div>
+                  <h1> 101
+                    <span>left</span>
+                  </h1>
+                </div>
+                <p> You get an ergonomic stand made of natural bamboo. You've helped us launch our promotional campaign, and
+    you’ll be added to a special Backer member list.</p> 
+              </div>
+        </div>
+        <div class="low com">
+          <input type="text" name="text" id="text" placeholder="Enter your pledge">
+          <div class="controls">
+              <div class="amount">
+                <p>$</p>
+                <h2>25</h2>
+              </div>
+              <button class="" id="con">Continue</button>
+          </div>
+      
+      </div>
+
+
+      </div>
+       <div class="top">
+          <div class="up com">
+              <div class="radio"></div>
+                <div class="info">
+                  <div class="pledge">
+                    <div class="more">
+                      <h2>Black Edition Stand</h2>
+                      <p>Pledge $75 or more</p>
+                    </div>
+                    <h1>64
+                      <span>left</span>
+                    </h1>
+                  </div>
+                  <p> You get a Black Special Edition computer stand and a personal thank you. You’ll be added to our Backer
+      member list. Shipping is included.</p> 
+                </div>
+          </div>
+          <div class="low com">
+            <input type="text" name="text" id="text" placeholder="Enter your pledge">
+            <div class="controls">
+                <div class="amount">
+                  <p>$</p>
+                  <h2>75</h2>
+                </div>
+                <button class="" id="con">Continue</button>
+            </div>
+      
+        </div>
+
+
+        </div>
+     <div class="top" id="buz">
+          <div class="up com">
+              <div class="radio"></div>
+                <div class="info">
+                  <div class="pledge">
+                    <div class="more">
+                      <h2>Mahogany Special Edition</h2>
+                      <p>Pledge $200 or more</p>
+                    </div>
+                    <h1>0
+                      <span>left</span>
+                    </h1>
+                  </div>
+                  <p>You get two Special Edition Mahogany stands, a Backer T-Shirt, and a personal thank you. You’ll be added
+    to our Backer member list. Shipping is included.</p> 
+                </div>
+          </div>
+          <div class="low com">
+            <input type="text" name="text" id="text" placeholder="Enter your pledge">
+            <div class="controls">
+                <div class="amount">
+                  <p>$</p>
+                  <h2>200</h2>
+                </div>
+                <button class="" id="con">Continue</button>
+            </div>
+      
+        </div>
+
+
+        </div>
+</div>
 </template>
 
 
 
 <style lang="scss" scoped>
 
+
+ button{
+        background: hsl(176, 50%, 47%);
+        border-radius: 30px;
+        padding: 1em 2.5em;
+        color: white;
+        font-family:'bold';
+        cursor: pointer;
+        &:hover{
+            background: hsl(176, 72%, 28%);
+        }
+}
 </style>
