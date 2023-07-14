7d29c34fa5d9013380f32a419b89e069db7bb672
githukelvin <kelvingithu019@gmail.com>
2023-07-14 18:15:51 +0300

    added vue

---
+++ src/components/PledgeCard.vue
@@ -0,0 +1,36 @@
+<script setup>
+
+</script>
+
+<template>
+ <div class="top">
+    <div class="up com">
+        <div class="radio">
+
+
+          </div>
+          <div class="info">
+          <h1></h1>
+          <p></p> 
+          </div>
+    </div>
+    <div class="low com">
+      <input type="text" name="text" id="text" placeholder="Enter your pledge">
+      <div class="controls">
+          <div class="amount">
+            <p>$</p>
+            <h2>25</h2>
+          </div>
+          <button class="" id="con">Continue</button>
+      </div>
+    </div>
+
+
+  </div>
+</template>
+
+
+
+<style lang="scss" scoped>
+
+</style>
